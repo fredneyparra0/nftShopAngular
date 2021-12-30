e7fb44afad71c60629e6c7229491620762296213
fredneyparra0 <fredneyparra0@gmail.com>
2021-12-30 14:50:59 -0400

    header integrate and table of products

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { FilterComponent } from './views/filter/filter.component';
+import { SharedModule } from '../shared/shared.module';
+import { TableProductsComponent } from './components/table-products/table-products.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    FilterComponent
+    FilterComponent,
+    TableProductsComponent,
+    CardProductComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
